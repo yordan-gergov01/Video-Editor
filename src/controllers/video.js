58abb53a2ec447f0bf4f20f59d447291407a948a
yordan-gergov01 <yordansoftskills@gmail.com>
2025-04-21 09:52:35 +0300

    implementing queue-based processing and scheduling the resizes

--- src/controllers/video.js
+++ src/controllers/video.js
@@ -5,15 +5,18 @@ const { pipeline } = require("node:stream/promises");
 const util = require("../../lib/util");
 const DB = require("../DB");
 const FF = require("../../lib/FF");
+const JobQueue = require("../../lib/JobQueue");
 
+const jobs = new JobQueue();
+
+// Return the list of all the videos that a logged in user has uploaded
 const getVideos = (req, res, handleErr) => {
-  const name = req.params.get("name");
+  DB.update();
+  const videos = DB.videos.filter((video) => {
+    return video.userId === req.userId;
+  });
 
-  if (name) {
-    res.json({ message: `Your name is ${name}` });
-  } else {
-    return handleErr({ status: 400, message: "Please specify a name." });
-  }
+  res.status(200).json(videos);
 };
 
 // Upload a video file
@@ -71,9 +74,139 @@ const uploadVideo = async (req, res, handleErr) => {
   }
 };
 
+// Extract the audio for a video file (can only be done once per video)
+const extractAudio = async (req, res, handleErr) => {
+  const videoId = req.params.get("videoId");
+
+  DB.update();
+  const video = DB.videos.find((video) => video.videoId === videoId);
+
+  if (video.extractedAudio) {
+    return handleErr({
+      status: 400,
+      message: "The audio has already been extracted for this video.",
+    });
+  }
+
+  try {
+    const originalVideoPath = `./storage/${videoId}/original.${video.extension}`;
+    const targetAudioPath = `./storage/${videoId}/audio.aac`;
+
+    await FF.extractAudio(originalVideoPath, targetAudioPath);
+
+    video.extractedAudio = true;
+    DB.save();
+
+    res.status(200).json({
+      status: "success",
+      message: "The audio was extracted successfully!",
+    });
+  } catch (e) {
+    util.deleteFile(targetAudioPath);
+    return handleErr(e);
+  }
+};
+
+// Resize a video file (creates a new video file)
+const resizeVideo = async (req, res, handleErr) => {
+  const videoId = req.body.videoId;
+  const width = Number(req.body.width);
+  const height = Number(req.body.height);
+
+  DB.update();
+  const video = DB.videos.find((video) => video.videoId === videoId);
+  video.resizes[`${width}x${height}`] = { processing: true };
+  DB.save();
+
+  jobs.enqueue({
+    type: "resize",
+    videoId,
+    width,
+    height,
+  });
+
+  res.status(200).json({
+    status: "success",
+    message: "The video is now being processed!",
+  });
+};
+
+// Return a video asset to the client
+const getVideoAsset = async (req, res, handleErr) => {
+  const videoId = req.params.get("videoId");
+  const type = req.params.get("type"); // thumbnail, original, audio, resize
+
+  DB.update();
+  const video = DB.videos.find((video) => video.videoId === videoId);
+
+  if (!video) {
+    return handleErr({
+      status: 404,
+      message: "Video not found!",
+    });
+  }
+
+  let file;
+  let mimeType;
+  let filename; // the final file name for the download (including the extension)
+
+  switch (type) {
+    case "thumbnail":
+      file = await fs.open(`./storage/${videoId}/thumbnail.jpg`, "r");
+      mimeType = "image/jpeg";
+      break;
+    case "audio":
+      file = await fs.open(`./storage/${videoId}/audio.aac`, "r");
+      mimeType = "audio/aac";
+      filename = `${video.name}-audio.aac`;
+      break;
+    case "resize":
+      const dimensions = req.params.get("dimensions");
+      file = await fs.open(
+        `./storage/${videoId}/${dimensions}.${video.extension}`,
+        "r"
+      );
+      mimeType = "video/mp4"; // Not a good practice! Videos are not always MP4
+      filename = `${video.name}-${dimensions}.${video.extension}`;
+      break;
+    case "original":
+      file = await fs.open(
+        `./storage/${videoId}/original.${video.extension}`,
+        "r"
+      );
+      mimeType = "video/mp4"; // Not a good practice! Videos are not always MP4
+      filename = `${video.name}.${video.extension}`;
+      break;
+  }
+
+  // Grab the file size
+  const stat = await file.stat();
+
+  const fileStream = file.createReadStream();
+
+  if (type !== "thumbnail") {
+    // Set a header to prompt for download
+    res.setHeader("Content-Disposition", `attachment; filename=${filename}`);
+  }
+
+  // Set the Content-Type header based on the file type
+  res.setHeader("Content-Type", mimeType);
+  // Set the Content-Length to the size of the file
+  res.setHeader("Content-Length", stat.size);
+
+  res.status(200);
+
+  await pipeline(fileStream, res);
+
+  file.close();
+};
+
 const controller = {
   getVideos,
   uploadVideo,
+  extractAudio,
+  resizeVideo,
+  getVideoAsset,
 };
 
 module.exports = controller;
--- src/router.js
+++ src/router.js
@@ -22,8 +22,19 @@ module.exports = (server) => {
   // ------------------------------------------------ //
   // ************ VIDEO ROUTES ************* //
   // ------------------------------------------------ //
-  server.route("get", "/api/videos-temp", Video.getVideos);
+
+  // Return the list of all the videos that a logged in user has uploaded
+  server.route("get", "/api/videos", Video.getVideos);
 
   // Upload a video file
   server.route("post", "/api/upload-video", Video.uploadVideo);
+
+  // Extract the audio for a video file (can only be done once per video)
+  server.route("patch", "/api/video/extract-audio", Video.extractAudio);
+
+  // Resize a video file (creates a new video file)
+  server.route("put", "/api/video/resize", Video.resizeVideo);
+
+  // Return a video asset to the client
+  server.route("get", "/get-video-asset", Video.getVideoAsset);
 };
